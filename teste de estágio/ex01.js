5b3f0787e45d98d0765f7a07020751d858b2f07a
futuroDevLeo <leonunes07@outlook.com>
2024-03-07 16:23:47 -0300

    feat: first challenge completed

---
+++ teste de estágio/ex01.js
@@ -0,0 +1,11 @@
+// #1) Ao final do processamento, qual será o valor da variável SOMA? R: SOMA = 91
+const indice = 13;
+let soma = 0;
+let k = 0;
+
+while (k < indice) {
+    k++
+    soma = soma + k
+}
+
+console.log(soma);
