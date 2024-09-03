49558781f15d45b419a80e59fb73e30571261a12
futuroDevLeo <leonunes07@outlook.com>
2024-09-03 01:27:48 -0300

    feat: complete test04.py with full implementation of the test

--- target-internship-tests/test04.py
+++ target-internship-tests/test04.py
@@ -8,3 +8,16 @@
 # Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado
 # teve dentro do valor total mensal da distribuidora.
 
+fat_mensal = {
+    'SP': 67836.43,
+    'RJ': 36678.66,
+    'MG': 29229.88,
+    'ES': 27165.48,
+    'Outros': 19849.53
+}
+
+total_mensal = sum(fat_mensal.values())
+
+for keys, values in fat_mensal.items():
+    rep = (values/total_mensal)*100
+    print(f'{keys}: {rep:.2f}%')
