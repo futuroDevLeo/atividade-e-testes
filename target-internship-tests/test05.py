c91ee7cc98f58e21ad9e3df2bf52ca0e3a6b3761
futuroDevLeo <leonunes07@outlook.com>
2024-09-03 02:03:20 -0300

    feat: complete test05.py with full implementation of the test

--- target-internship-tests/test05.py
+++ target-internship-tests/test05.py
@@ -4,3 +4,11 @@
 # a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 # b) Evite usar funções prontas, como, por exemplo, reverse;
 
+def invert_word(w: str):
+    new_w = ''
+    for letter in w:
+        new_w = letter + new_w
+    print(new_w)
+
+word = input('Digite uma palavra: ')
+invert_word(word)
