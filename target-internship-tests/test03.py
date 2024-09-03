f186f8650ea52cfdf710ad867344b5f41f78262d
futuroDevLeo <leonunes07@outlook.com>
2024-09-03 17:53:45 -0300

    feat: add dados.json and complete implementation of test03.py

--- target-internship-tests/test03.py
+++ target-internship-tests/test03.py
@@ -6,4 +6,46 @@
 
 # IMPORTANTE:
 # a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
-# b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
+# b) Podem existir dias sem faturamento, como nos finais de semana e feriados.
+# Estes dias devem ser ignorados no cálculo da média;
+
+import json
+
+file = 'dados.json'
+
+try:
+    with open(file, 'rt') as file:
+        data_JSON = file.read()
+    data = json.loads(data_JSON)
+except FileExistsError:
+    print(f"O arquivo '{file}' não foi encontrado.")
+except json.JSONDecodeError:
+    print("Erro ao decodificar o JSON.")
+
+revenues = []
+days_above_average = 0
+min_revenue = float('inf')
+max_revenue = float('-inf')
+
+for item in data:
+    values = list(item.values())
+    day = values[0]
+    revenue = values[1]
+
+    if revenue > 0.0:
+        if revenue > max_revenue:
+            max_revenue = revenue
+
+        if revenue < min_revenue:
+            min_revenue = revenue
+        
+        revenues.append(revenue)
+
+average_revenue = sum(revenues) / len(revenues)
+for value in revenues:
+    if value > average_revenue:
+        days_above_average += 1
+
+print("Maior faturamento:", max_revenue)
+print("Menor faturamento:", min_revenue)
+print('Quantidade de dias com faturamento acima da média:', days_above_average)
